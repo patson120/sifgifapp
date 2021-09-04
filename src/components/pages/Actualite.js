3232e1a302b68e482164ea9c459c20c5e600f52c
patson120 <patrickkenne@gmail.com>
2021-09-04 17:10:40 +0200

    Mise à jour du responsive sur les différentes actualité de la page d'actualités

--- src/components/pages/Actualite.js
+++ src/components/pages/Actualite.js
@@ -3,10 +3,17 @@ import { Menu } from './Menu';
 import '../css/actualite.css';
 import { Link } from 'react-router-dom';
 
-
-
+const actualites = [
+    { id: 1, ressource: "Actualité", link: "#", src: "./images/pexels-photo-1112186.jpeg", description: "A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon" },
+    { id: 2, ressource: "Vidéo", link: "#", src: "./images/images.jpg", description: "Agir pour la biodiversité : un engagement au quotidien à l'ONF, 7 actions concrètes" },
+    { id: 3, ressource: "Vidéo", link: "#", src: "./images/images_1.jpg", description: "Rapport d'activité 2020 de l'ONF" },
+    { id: 4, ressource: "Rapport", link: "#", src: "./images/bg.jpg", description: "“Nos équipes DFCI sont très mobilisées dans le sud de la France, et restent vigilantes dans les…" },
+    { id: 5, ressource: "Interview", link: "#", src: "./images/images_2.jpg", description: "“Raconte-moi la forêt” : la page qui vous conte la forêt et ses secrets" },
+    { id: 6, ressource: "Actualité", link: "#", src: "./images/images_4.jpg", description: "Concours photo : #DestinationForêt, à vos clichés !" },
+]
 
 export const Actualite = () => {
+
     return (
         <div className='actualite'>
             <div>
@@ -25,72 +32,19 @@ export const Actualite = () => {
                     <Link to="#" className="block-2"></Link>
                 </div>
                 <div className="type-actu">
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="actu">
-                        <p className="ressource">Vidéo</p>
-                        <Link className="link" to="#">
-                            <div className="image">
-                                <img src='./images/pexels-photo-1112186.jpeg' alt="" />
-                            </div>
-                            <div className="text-content">
-                                <p>A l'ombre de Quercus, rencontre avec le forestier et auteur Laurent Tillon</p>
-                            </div>
-                        </Link>
-                    </div>
+                    {actualites.map(actualite => (
+                        <div key={actualite.id} className="actu">
+                            <p className="ressource">{actualite.ressource}</p>
+                            <Link className="link" to={actualite.link}>
+                                <div className="image">
+                                    <img src={actualite.src} alt="" />
+                                </div>
+                                <div className="text-content">
+                                    <p>{actualite.description}</p>
+                                </div>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
