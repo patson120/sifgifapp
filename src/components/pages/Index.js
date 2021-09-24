d2b378defc890a54e22f0b4c138ff3099b5f1537
patson120 <patrickkennenl@gmail.com>
2021-09-24 11:32:17 +0100

    Changement des images

--- src/components/pages/Index.js
+++ src/components/pages/Index.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 export const Index = () => {
     return (
         <div className="Index">
-            <div className="container-fluid rounded-pill" style={{ padding: '10px', background: 'green' }}>
-                <div className="rounded-pill" style={{ padding: '10px', background: 'red' }}>
-                    <div className="rounded-pill" style={{ padding: '10px', background: 'yellow' }}>
+            <div className="container-fluid rounded-pill" style={{ padding: '8px', background: 'green' }}>
+                <div className="rounded-pill" style={{ padding: '8px', background: 'red' }}>
+                    <div className="rounded-pill" style={{ padding: '8px', background: 'yellow' }}>
                         <div className="block-accueil rounded-pill container-fluid position-relative d-md-flex justify-content-between">
                             <h2 className="h4 text-white mx-5 align-text-center w-25 h-100">SYSTEME INFORMATIQUE DE GESTION DES INFORMATIONS FORESTIERES</h2>
                             <h2 className="h4 text-white mx-5 align-text-center w-25 h-100">FOREST INFORMATION MANAGEMENT COMPUTER SYSTEM</h2>
